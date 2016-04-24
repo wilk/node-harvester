00c8feb78f4308a2a004464a6e276b332892ce02
Vincenzo (Wilk) Ferrari <wilk3ert@gmail.com>
2016-04-24 23:25:43 +0200

    improved clients test

--- test/clients.test.ts
+++ test/clients.test.ts
@@ -7,85 +7,80 @@ let expect: ExpectStatic = chai.expect,
     harvestCfg: IHarvestConfig = config.get<IHarvestConfig>('harvest'),
     harvest: Harvest = new Harvest(harvestCfg),
     clientConfig: IClient = {
-        name: 'Test Client',
+        name: 'An Example Ltd',
         currency: 'Europe Euro - EUR',
         currency_symbol: '€',
         details: '123 Via XXV Aprile, Italy 12345'
     },
     clientId: number;
 
-describe('Harvest Clients', function (): void {
-    it('should create a new client', async function (done: Function): Promise<void> {
+let errorHandlerBuilder: Function = function (done: Function, status: number): Function {
+    return function (err: Error|any): void {
+        if (err instanceof Error) return done(err);
         try {
-            let client: IClient = await harvest.clients.create(clientConfig);
-
-            expect(client).to.be.not.null;
-            expect(client.id).to.be.a('number');
-            expect(client.name).to.equal(clientConfig.name);
-            expect(client.currency_symbol).to.equal(clientConfig.currency_symbol);
-            clientId = client.id;
-            done();
-        }
-        catch (err) {
-            console.log(err);
-            done(err);
-        }
-    });
-
-    it('should return the clients list', async function (done: Function): Promise<void> {
-        try {
-            let clients: IClient[]|Error = await harvest.clients.list();
-
-            expect(clients).to.be.not.null;
-            expect(clients).to.have.length.above(0);
-            done();
-        }
-        catch (err) {
-            expect(err).to.be.null;
-            done(err);
-        }
-    });
-
-    it('should return the created client', async function (done: Function): Promise<void> {
-        try {
-            let client: IClient = await harvest.clients.single(clientId);
-
-            expect(client).to.be.not.null;
-            expect(client.id).to.be.a('number');
-            expect(client.name).to.equal(clientConfig.name);
-            expect(client.currency_symbol).to.equal(clientConfig.currency_symbol);
-            done();
-        }
-        catch (err) {
-            expect(err).to.be.null;
-            done(err);
-        }
-    });
-
-    xit('should toggle the created client', async function (done: Function): Promise<void> {
-        try {
-            await harvest.clients.toggle(clientId);
-            done();
-        }
-        catch (err) {
-            expect(err).to.be.null;
-            done(err);
-        }
-    });
-
-    it('should delete the created client', async function (done: Function): Promise<void> {
-        try {
-            await harvest.clients.delete(clientId);
-            let client: IClient = await harvest.clients.single(clientId);
-
-            expect(client).to.be.null;
-            done();
-        }
-        catch (err) {
-            expect(err).to.be.not.null;
             expect(err).to.be.not.instanceOf(Error);
-            expect(err.status).to.be.equal(404);
+            expect(err.status).to.not.equal(status);
+            done();
+        }
+        catch (err) {
             done(err);
         }
+    };
+};
+
+describe('Harvest Clients', function (): void {
+    it('should create a new client', function (done: Function): void {
+        harvest.clients.create(clientConfig)
+            .then((client: IClient): void => {
+                expect(client).to.be.not.null;
+                expect(client.id).to.be.a('number');
+                expect(client.name).to.equal(clientConfig.name);
+                expect(client.currency_symbol).to.equal(clientConfig.currency_symbol);
+                clientId = client.id;
+                done();
+            })
+            .catch(errorHandlerBuilder(done, 200));
+    });
+
+    it('should return the clients list', function (done: Function): void {
+        harvest.clients.list()
+            .then((clients: IClient[]|Error): void => {
+                expect(clients).to.be.not.null;
+                expect(clients).to.have.length.above(0);
+                done();
+            })
+            .catch(errorHandlerBuilder(done, 200));
+    });
+
+    it('should return the created client', function (done: Function): void {
+        harvest.clients.single(clientId)
+            .then(function (client: IClient): void {
+                expect(client).to.be.not.null;
+                expect(client.id).to.be.a('number');
+                expect(client.name).to.equal(clientConfig.name);
+                expect(client.currency_symbol).to.equal(clientConfig.currency_symbol);
+                done();
+            })
+            .catch(errorHandlerBuilder(done, 200));
+    });
+
+    xit('should toggle the created client', function (done: Function): void {
+        harvest.clients.toggle(clientId)
+            .then((): void => {
+                done();
+            })
+            .catch(errorHandlerBuilder(done, 200));
+    });
+
+    it('should delete the created client', function (done: Function): void {
+        harvest.clients.delete(clientId)
+            .then((): Promise<IClient|Error> => {
+                return harvest.clients.single(clientId);
+            })
+            .then((client: IClient): void => {
+                expect(client).to.be.null;
+                done();
+            })
+            .catch(errorHandlerBuilder(done, 404));
     });
 });
